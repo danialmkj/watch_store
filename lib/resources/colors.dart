73683ed00152d99b2c3a5cc329709c8de19e7a1b
danialmjk <danimalekdev@gmail.com>
2023-08-15 16:48:49 +0330

    initial commit

---
+++ lib/resources/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+
+  static const Color title = Colors.black;
+  static const Color hintColor = Color.fromARGB(255, 217, 220, 228);
+  static const Color appBarColor = Color.fromARGB(255, 217, 220, 228);
+  static const Color borderColor = Color.fromARGB(255, 217, 220, 228);
+  static const Color focuseBorderColor = Color.fromARGB(255, 0, 217, 251);
+  static const Color buttonColor = Color.fromARGB(255, 0, 217, 251);
+  static const Color surfaceColor = Color.fromARGB(255, 243, 243, 243);
+
+
+
+
+}
---
+++ lib/resources/dimens.dart
@@ -0,0 +1,8 @@
+abstract class Dimens{
+
+  
+
+  static const int small = 8 ;
+  static const int medium = 12 ;
+  static const int large = 24 ;
+}
